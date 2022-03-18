874e92d75b9ca4b60e6bf388482e9ed5f8d0b96d
Davi Santos <davi.santos@shiftpixy.com>
2022-03-17 23:57:32 -0300

    feature(iOS): implemented the native settings and starting map

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { Text } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 import { Container } from '~/screens/Home/HomeScreen.styles'
 
 const HomeScreen = () => {
   return (
     <Container>
-      <Text>XDDDDXDDDDXDDDDXDDDDXDDDDXDDDDXDDDDXDDDDXDDDDXDDDDXDDDDXDDDD</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </Container>
   )
 }
