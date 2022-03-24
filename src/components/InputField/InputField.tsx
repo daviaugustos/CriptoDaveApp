2dd3f98d9b4d517d60fd0d405106dfb247516146
Davi Santos <davi.santos@shiftpixy.com>
2022-03-24 15:04:44 -0300

    feat(icons): added the icons setup

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import { Container, Input, InputIconView } from './InputField.styles'
 
 const InputField = () => {
   return (
     <Container>
-      <InputIconView />
+      <InputIconView>
+        <Icon name="rocket" size={30} color="#900" />
+      </InputIconView>
       <Input />
     </Container>
   )
